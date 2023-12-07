9f86131683ad060e74f2fa607f5b22bffb993570
KESAVAGAJJALA <115894976+KESAVAGAJJALA@users.noreply.github.com>
2023-12-07 11:28:49 +0530

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,13 +1,1 @@
-#creating key pair key.tf
-resource "aws_key_pair" "master" {
-key_name = "master"
-public_key = tls_private_key.rsa.public_key_openssh
-}
-resource "tls_private_key" "rsa" {
-algorithm = "RSA"
-rsa_bits =4096
-}
-resource "local_file" "tf-key" {
-content = tls_private_key.rsa.private_key_pem
-filename = "master"
-}
+
