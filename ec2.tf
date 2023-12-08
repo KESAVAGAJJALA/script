9edee73bf09afa7fe8d90e9291a288143c9647ec
KESAVAGAJJALA <115894976+KESAVAGAJJALA@users.noreply.github.com>
2023-12-08 15:18:38 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
-  ami                         = "ami-0d92749d46e71c34c"
+  ami                         = "ami-0fa1ca9559f1892ec"
   instance_type               = "t2.micro"
-  key_name                    = "master"
+  key_name                    = "jen"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
   subnet_id                   = aws_subnet.public-subnet-1.id
   associate_public_ip_address = true
