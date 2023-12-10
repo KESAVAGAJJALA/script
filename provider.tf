fd452ff1372abe6f98f20ddf08f89b5928fe2f35
KESAVAGAJJALA <115894976+KESAVAGAJJALA@users.noreply.github.com>
2023-12-10 22:37:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region     = "eu-west-2"
-    access_key = "AKIA3CJ3EQLA3U5BYU7H"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
